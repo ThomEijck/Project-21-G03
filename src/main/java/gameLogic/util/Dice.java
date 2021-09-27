b451596abfb47868c8173136241b0a6cd4ad2f53
WillemP1 <willem.ploegstra@gmail.com>
2021-09-27 16:13:29 +0200

    Dice updated to handle pawn promotion

--- src/main/java/gameLogic/util/Dice.java
+++ src/main/java/gameLogic/util/Dice.java
@@ -27,10 +27,19 @@ public class Dice
             for (int j = 0; j < pieces.length; j++)
             {
                 Piece piece = pieces[i][j];
+                Position[] moves = piece.findMoves(pieces);
                 int index = nameToNum(piece.toString());
                 //check if possible moves can be made
-                if(index >= 0 && availablePieces[index - 1] == 0 && hasValidMoves(piece.findMoves(pieces)))
+                if(index >= 0 && availablePieces[index - 1] == 0 && hasValidMoves( moves))
                 {
+                    if(index == 1 && canPromote(moves))
+                    {
+                        //all pieces except king can be rolled if a pawn can be promoted
+                        for (int k = 0; k < moves.length - 1; k++)
+                        {
+                            availablePieces[index - 1] = 1;
+                        }
+                    }
                     availablePieces[index - 1] = 1;
                     count++;
                 }
@@ -58,11 +67,23 @@ public class Dice
         return piece;
     }
 
+    private boolean canPromote(Position[] moves) {
+        for (Position position : moves) {
+            if(position.row == 7 || position.row == 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     //function to check if a piece has valid moves
     public boolean hasValidMoves(Position[] moves)
     {
         for (int i = 0; i < moves.length; i++) 
         {
+            //invalid moves will have the target -1,-1 
+            //So to check validity we just have to check if the target row != -1
             if(moves[i].row >= 0)
             {
                 return true;

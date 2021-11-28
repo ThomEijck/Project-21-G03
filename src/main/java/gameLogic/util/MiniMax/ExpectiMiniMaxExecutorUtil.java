788c84fede926def6a5d1ad91ece4e35a22dfdc2
Jiska <jiska.beuk@gmail.com>
2021-11-28 20:51:20 +0100

    Implemented Expectiminimax

---
+++ src/main/java/gameLogic/util/MiniMax/ExpectiMiniMaxExecutorUtil.java
@@ -0,0 +1,158 @@
+package gameLogic.util.MiniMax;
+
+import gameLogic.util.*;
+import gameLogic.pieces.*;
+import java.util.*;
+
+public class ExpectiMiniMaxExecutorUtil {
+    private BoardEvaluatorUtil evaluator;
+    private MoveExecutorUtil moveExecutor;
+
+
+    public ExpectiMiniMaxExecutorUtil(BoardEvaluatorUtil evaluator, MoveExecutorUtil moveExecutor){
+        this.evaluator = evaluator;
+        this.moveExecutor = moveExecutor;
+    }
+
+
+    public Move findBestMove(Board currentBoard, int player, int depth, int diceValue){
+
+        // first iteration of minimax done separately, to use the value of the dice
+        float bestValue = Integer.MIN_VALUE;
+        Move bestMove = null;
+        Move[] moves = getMoves(currentBoard, diceValue, player);
+        for(int i = 0; i < moves.length; i++){
+            if(moves[i].getEnd().row == -1)
+                continue;
+            //System.out.println(i);
+            moveExecutor.movePiece(currentBoard, moves[i]);
+            float newValue = minimax(currentBoard, depth - 1, nextPlayer(player));
+            moveExecutor.unMovePiece(currentBoard, moves[i]);
+            if (newValue > bestValue){
+                bestValue = newValue;
+                bestMove = moves[i];
+            }
+        }
+        return bestMove;
+    }
+    //NOTE: if you want to turn this into a expectiminimax you need an extra parameter for dice value
+    private float minimax(Board board, int depth, int currentPlayer){
+        if (depth == 0){
+            return evaluator.evaluateBoard(board);
+        }
+        //board.printBoard();
+        List<Move>[] movesList = new List[6];
+        for(int i = 0; i<6; i++){
+            movesList[i] = new ArrayList<Move>();
+        }
+
+        for (int i = 1; i <= 6; i++) {//get moves of all pieces
+            addMoves(movesList[i-1],board,i,currentPlayer);
+        }
+
+        if (currentPlayer == 1){
+            float totalValue = 0;
+            int possibleDiceValues = 0;
+            for(int diceValue = 1; diceValue <=6; diceValue++) {
+                Move[] moves = movesList[diceValue-1].toArray(new Move[0]);
+
+                if (moves.length > 0){
+                    possibleDiceValues++;
+                    float value = Integer.MIN_VALUE;
+                    for (int i = 0; i < moves.length; i++) {
+                        if (moves[i].getEnd().row == -1)
+                            continue;
+                        moveExecutor.movePiece(board, moves[i]);
+                        value = Float.max(value, minimax(board, depth - 1, nextPlayer(currentPlayer)));
+                        moveExecutor.unMovePiece(board, moves[i]);
+                    }
+                    totalValue += value;
+                }
+            }
+            if(possibleDiceValues>0) {
+                return totalValue / possibleDiceValues;
+            }
+            else {
+                return 0;//its a draw
+            }
+        }
+        else{
+            float totalValue = 0;
+            int possibleDiceValues = 0;
+            for(int diceValue = 1; diceValue <=6; diceValue++) {
+                Move[] moves = movesList[diceValue-1].toArray(new Move[0]);
+
+                if (moves.length > 0){
+                    possibleDiceValues++;
+                    float value = Integer.MAX_VALUE;
+                    for (int i = 0; i < moves.length; i++) {
+                        if (moves[i].getEnd().row == -1)
+                            continue;
+                        moveExecutor.movePiece(board, moves[i]);
+                        value = Float.min(value, minimax(board, depth - 1, nextPlayer(currentPlayer)));
+                        moveExecutor.unMovePiece(board, moves[i]);
+                    }
+                    totalValue += value;
+                }
+            }
+            if(possibleDiceValues>0) {
+                return totalValue / possibleDiceValues;
+            }
+            else {
+                return 0;//its a draw
+            }
+        }
+    }
+
+    private Move[] getMoves(Board board, int diceValue, int player)
+    {
+        Piece[][] pieces = board.getChessBoard();
+        List<Move> moves = new ArrayList<Move>();
+        for (int i = 0; i < pieces.length; i++)
+        {
+            for (int k = 0; k < pieces[i].length; k++)
+            {
+
+                Piece piece = pieces[i][k];
+                if(piece == null){continue;}
+                if((piece.getInt() != diceValue && piece.getInt() != 1) || piece.getPlayer() != player)
+                {
+                    continue;//only get the relevant pieces
+                }
+                Position[] targets = piece.findMoves(board.getChessBoard());
+                for (int l = 0; l < targets.length; l++)
+                {
+                    if(piece.getInt() == 1 && !(targets[l].row == 0 || targets[l].column == 7))
+                    {
+                        //if its a pawn and we cant promote we cant move it
+                        continue;
+                    }
+                    //add the move to the list
+                    Position pos = new Position(piece.getPos());
+                    moves.add(new Move(pos,targets[l]));
+                }
+            }
+        }
+        return moves.toArray(new Move[0]);//not sure if this works
+    }
+
+
+    private int nextPlayer(int currPlayer){
+        if (currPlayer == 1){
+            return 2;
+        }
+        else return 1;
+    }
+
+    private void addMoves(List<Move> moves,Board board, int diceValue,int player)
+    {
+        Move[] pieceMoves = getMoves(board,diceValue,player);
+        for (int i = 0; i < pieceMoves.length; i++)
+        {
+            moves.add(pieceMoves[i]);
+        }
+    }
+
+
+}
+
